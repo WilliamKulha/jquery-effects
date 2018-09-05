f0722ccd10ae84432cfe1ac99724703b1e84e457
William Kulha <kulha.william@gmail.com>
2018-09-05 15:44:54 -0400

    Make buttons at the top of the page functional for sliding down panels.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@ $(document).ready(function (){
   $("#cuteclick").click(function(){
     $("#cutepanel").slideDown();
   });
+  $("#trashclick").click(function(){
+    $("#trashpanel").slideDown();
+  });
+  $("#worldclick").click(function(){
+    $("#worldpanel").slideDown();
+  });
+
 
 });
